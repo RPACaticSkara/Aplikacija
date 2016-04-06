f4d3b7a25eb3f40b2435d48d00da2c923a1dd2d3
mcatic23 <mcatic23@gmail.com>
2016-04-06 21:55:20 +0200

    Added files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AplikacijaZaEvidencijuKupaca.Startup))]
+namespace AplikacijaZaEvidencijuKupaca
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
